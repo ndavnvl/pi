f794867d2090fe5cc7a9dc8673515269daab0d1e
Nikolay Davydov <davnvl2000@gmail.com>
2019-02-26 08:21:28 +0700

    Some grammatical corrections

--- Pi/main.c
+++ Pi/main.c
@@ -5,10 +5,10 @@ int main() {
 	printf("This programm calculates pi with input accuracy.\n");
 	for (int cont = 1; cont;) {
 		int NumOfSigns = 0;
-		printf("Input number of sings after point in pi:\n");
+		printf("Input the number of sings after point in pi:\n");
 		scanf_s("%d", &NumOfSigns);
 		if (NumOfSigns < 0) {
-			printf("You input invalid number. It sould be more then 0. Try again...");
+			printf("You input invalid number. It sould be more than 0. Try again...");
 		}
 		else {
 			long double pi = 0;
